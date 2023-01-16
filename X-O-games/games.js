5215a4b49792f968d07be9b8f96547d3e58d3bdc
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2023-01-16 17:17:39 -0500

    Added files

---
+++ X-O-games/games.js
@@ -0,0 +1,442 @@
+
+/*basic game rules:
+	play on a rectangular board made of squares
+	boards are represented as a string that is length n, where the board is size p, q and p * q = n
+	two players
+	take turns placing one piece at a time
+	there must be some way to evaluate board utility
+*/
+
+
+
+
+//everything is an object and you are not going to remember inheritance correctly
+class Game {
+	constructor() {
+		this.dims = [1, 1, 1];
+		this.pieces = "XO";
+		this.players = [this.pieces[0], this.pieces[1]];
+		this.thinkDist = -1;
+		this.utilGoals = {
+			"X": 1,
+			"O": -1
+		};
+	}
+
+	applyMove(boardState, location, piece) {
+		return boardState.slice(0, location) + piece + boardState.slice(location + 1);
+	}
+
+	createBoard() {
+		return boardEmpty.repeat(this.dims[0] * this.dims[1]);
+	}
+
+	gameIsOver(boardState) {
+		//a game is generally over if it gives utility
+		return (this.utilityFor(boardState) != undefined);
+	}
+
+	generateMoveTree(boardState, maxDepth, player) {
+		var selfNode = {
+			board: boardState,
+			children: [],
+			utility: undefined
+		};
+
+		//if the game is over or it's being cut off, evaluate utility
+		if (maxDepth <= 0 || this.gameIsOver(boardState)) {
+			selfNode.utility = this.utilityFor(boardState) ?? 0;
+			return selfNode;
+		}
+	
+		//if unfinished, recurse
+		var minUtilDist = 1e1001;
+		var newNode;
+		//figure out children
+		var children = this.constructChildren(selfNode.board, player);
+	
+		for (var c=0; c<children.length; c++) {
+			if (children[c] != undefined) {
+				newNode = this.generateMoveTree(children[c][1], maxDepth - 1, this.incrementPlayer(player));
+				selfNode.children.push(newNode);
+	
+				//if that possible utility is closer to self's goal, then make that the utility for the boare
+				if (Math.abs(newNode.utility - this.utilGoals[player]) < minUtilDist) {
+					minUtilDist = Math.abs(newNode.utility - this.utilGoals[player]);
+					selfNode.utility = newNode.utility;
+	
+					//if it's the closest possible, it's not worth continuing (can't find a closer state)
+					// if (minUtilDist == 0) {
+					// 	return selfNode;
+					// }
+				}
+			}
+		}
+	
+		return selfNode;
+	}
+
+	incrementPlayer(player) {
+		return (player == this.pieces[1]) ? this.pieces[0] : this.pieces[1];
+	}
+
+	utilityFor(board) {
+		return this.utilityStandard(board, this.dims[0], this.dims[1], this.dims[2]);
+	}
+
+	utilityStandard(boardState, width, height, requiredInARow) {
+		var valuesFromBoard = [];
+		var inARow = 0;
+		var rowVal;
+	
+		for (var c=0; c<boardState.length; c++) {
+			valuesFromBoard[c] = +(boardState[c] == this.pieces[0]) - +(boardState[c] == this.pieces[1])
+		}
+	
+	
+		/*
+		Here's the dealio. From each square, there are four possible ways to make a winning line.
+		Down, across, diagonal left, and diagonal right.
+		The other four directions aren't counted because they're duplicates, and given that we're starting from the top of the board, will just be discovered as one of the other four.
+	
+		So the algorithm loops through the portion of the board that can make one of those four lines, and tries to see if there are any lines there. If there are, returns the utility for those.
+		*/
+	
+		//check all the horizontal lines first, my little system doesn't work with those
+		for (var c=0; c<height; c++) {
+			for (var r=0; r<width; r++) {
+				if (valuesFromBoard[(width * c) + r] != 0 && valuesFromBoard[(width * c) + r] == rowVal) {
+					inARow += 1;
+					if (inARow >= requiredInARow) {
+						return rowVal;
+					}
+				} else {
+					inARow = 1;
+					rowVal = valuesFromBoard[(width * c) + r];
+				}
+			}
+			rowVal = undefined;
+		}
+	
+		for (r=0; r<width; r++) {
+			for (c=0; c<=height-requiredInARow; c++) {
+				//can't make a line if the start isn't a valid square
+				if (valuesFromBoard[r + (c * width)] != 0) {
+					//console.log(r, c);
+					//can self make a downwards line? (the answer is always yes, the height adjustment is taken care of in the initial for loop)
+					rowVal = valuesFromBoard[r + (c * width)];
+					for (var k=1; k<requiredInARow; k++) {
+						if (valuesFromBoard[r + ((c + k) * width)] != rowVal) {
+							k = requiredInARow + 10;
+						}
+					}
+					if (k == requiredInARow) {
+						return rowVal;
+					}
+	
+					//can this square make a DR line?
+					if (r <= width - requiredInARow) {
+						//console.log('could be DR');
+						//can this square make a DR line?
+						rowVal = valuesFromBoard[r + (c * width)];
+						for (var k=1; k<requiredInARow; k++) {
+							if (valuesFromBoard[r + k + ((c + k) * width)] != rowVal) {
+								k = requiredInARow + 10;
+							}
+						}
+						if (k == requiredInARow) {
+							return rowVal;
+						}
+					}
+	
+					//can this square make a DL line?
+					if (r >= requiredInARow - 1) {
+						//console.log('could be DL');
+						rowVal = valuesFromBoard[r + (c * width)];
+						for (var k=1; k<requiredInARow; k++) {
+							if (valuesFromBoard[r - k + ((c + k) * width)] != rowVal) {
+								k = requiredInARow + 10;
+							}
+						}
+						if (k == requiredInARow) {
+							return rowVal;
+						}
+					}
+				}
+			}
+		}
+		//if there are no moves left to play, it's a draw
+		if (boardState.indexOf(boardEmpty) == -1) {
+			return 0;
+		}
+	}
+}
+
+//actual games
+class ConnectFour extends Game {
+	constructor() {
+		super();
+		this.dims = [7, 6, 4];
+		this.thinkDist = 6;
+	}
+
+	constructChildren(boardState, player) {
+		//one command for each row
+		var sz = this.dims;
+		var buffer1 = [];
+
+		for (var x=0; x<sz[0]; x++) {
+			//is the row open?
+			if (boardState[x] == boardEmpty) {
+				//move down util found one
+				for (var y=1; y<sz[1]; y++) {
+					if (boardState[(sz[0] * y) + x] != boardEmpty) {
+						buffer1[x] = [boardChars[x], this.applyMove(boardState, (sz[0] * (y - 1)) + x, player)];
+						y = sz[1] + 1;
+					}
+				}
+
+				//if out of zone, set to last square
+				if (y < sz[1] + 1) {
+					buffer1[x] = [boardChars[x], this.applyMove(boardState, (sz[0] * (sz[1] - 1)) + x, player)];
+				}
+			}
+		}
+		return buffer1;
+	}
+}
+
+
+class FiveInARow extends Game {
+	constructor() {
+		super();
+		this.dims = [10, 10, 5];
+		this.thinkDist = 4;
+	}
+
+	//returns a list of name coordinates associated with board states
+	//one element example: ["23", "...XO..X..O...XX..O..O"]
+	constructChildren(boardState, player) {
+		var sz = this.dims;
+		var arr = [];
+		for (var y=0; y<sz[1]; y++) {
+			for (var x=0; x<sz[0]; x++) {
+				if (boardState[(y * sz[1]) + x] == boardEmpty) {
+					arr[x + (sz[1] * y)] = [boardChars[x] + boardChars[y], this.applyMove(boardState, x + (sz[1] * y), player)];
+				}
+			}
+		}
+		return arr;
+	}
+}
+
+
+class OrderAndChaos extends Game {
+	constructor() {
+		super();
+		this.dims = [6, 6, 5];
+		this.pieces = "xo"
+		this.players = ["order", "chaos"];
+	}
+
+	utilityFor(board) {
+		return Math.abs(super.utilityFor(board));
+	}
+
+	constructChildren(boardState, player) {
+		var sz = this.dims;
+		var arr = [];
+		for (var y=0; y<sz[1]; y++) {
+			for (var x=0; x<sz[0]; x++) {
+				if (boardState[(y * sz[1]) + x] == boardEmpty) {
+					arr[x + (sz[1] * y)] = [boardChars[x] + boardChars[y], this.applyMove(boardState, x + (sz[1] * y), this.pieces[0])];
+					arr[x + (sz[1] * y) + (sz[0] * sz[1])] = [boardChars[x] + boardChars[y], this.applyMove(boardState, x + (sz[1] * y), this.pieces[1])];
+				}
+			}
+		}
+		return arr;
+	}
+
+
+	/*
+	"order-and-chaos": {
+		constructChildren: (boardState, player) => {
+			var buffer1 = [];
+
+			//add all available squares
+			for (var c=0; c<boardState.length; c++) {
+				if (boardState[c] == ".") {
+					buffer1[c] = [boardChars[c], c];
+				}
+			}
+			return buffer1;
+		},
+		dims: [6, 6],
+		players: ["order", "chaos"],
+		thinkDist: 5,
+		utilGoals: {
+			"order": 1,
+			"chaos": 0,
+		}
+	} */
+}
+
+
+class Othello extends Game {
+	constructor() {
+		super();
+		this.dims = [10, 10];
+		this.pieces = "xo";
+		this.thinkDist = 6;
+		this.utilGoals = {
+			'x': 1e7,
+			'o': -1e7,
+		}
+		this.dirs = [-11, -10, -9, -1, 1, 9, 10, 11];
+	}
+
+	//returns a new board with a piece applied at an index. Flips pieces accordingly.
+	applyMove(board, index, piece, opposition) {
+		//apply initial piece
+		board = board.slice(0, index) + piece + board.slice(index+1);
+	
+		//apply changes in all directions
+		var trackIndex = index;
+		for (var dir of this.dirs) {
+			trackIndex = index + dir;
+			//go through board line until a different character is found
+			while (board[trackIndex] == opposition) {
+				trackIndex += dir;
+			}
+	
+			//if that different character is a good character, run back through and color all the pieces in between
+			if (board[trackIndex] == piece) {
+				trackIndex -= dir;
+				while (trackIndex != index) {
+					board = board.slice(0, trackIndex) + piece + board.slice(trackIndex+1);
+					trackIndex -= dir;
+				}
+			}
+		}
+		return board;
+	}
+
+	childIsGood(boardState, player, opposer, moveIndex) {
+		//loop through all directions to see if a swap is possible, if a swap can be made then the move is legal
+		var trackIndex = moveIndex;
+		for (var dir of this.dirs) {
+			trackIndex = moveIndex + dir;
+			while (boardState[trackIndex] == opposer) {
+				trackIndex += dir;
+			}
+			//if the run can flip a piece, say it's good
+			if (trackIndex != moveIndex + dir && boardState[trackIndex] == player) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	createBoard() {
+		//to be honest it's just easier to hardcode this one rather than having a bunch of repeat functions and variables.
+		return "```````````........``........``........``...xo...``...ox...``........``........``........```````````";
+	}
+
+	constructChildren(boardState, player) {
+		var opposite = (player == 'x') ? 'o' : 'x';
+		var moveList = [];
+
+		var nowIndex;
+		for (var c=1; c<9; c++) {
+			for (var r=1; r<9; r++) {
+				//if it's a blank, maybe a move can be made there
+				nowIndex = (c * 10) + r;
+				if (boardState[nowIndex] == boardEmpty) {
+					//if given the ok, add the move to the list
+					if (this.childIsGood(boardState, player, opposite, nowIndex)) {
+						moveList[(c * 10) + r] = [boardChars[r] + boardChars[c], this.applyMove(boardState, (c * 10) + r, player, opposite)];
+					}
+				}
+			}
+		}
+
+		return moveList;
+	}
+
+	gameIsOver(boardState) {
+		return (boardState.indexOf(boardEmpty) == -1);
+	}
+
+	utilityFor(boardState) {
+		//constants for adjusting how much the AI cares about different things
+		var utilWin = 1e7;
+		var utilCorner = 1e5;
+		var utilMove = 1e2;
+		var utilCapture = 1;
+
+		var numXs = (boardState.match(/x/g) || []).length;
+		var numOs = (boardState.match(/o/g) || []).length;
+
+		//if it's a win state, calculation is easy
+		if (numXs + numOs == 64) {
+			return utilWin * Math.sign(numXs - numOs);
+		}
+
+		var utility = 0;
+		//corners matter
+		utility += utilCorner * ((boardState[11] == 'x') + (boardState[18] == 'x') + (boardState[81] == 'x') + (boardState[88] == 'x'));
+		utility -= utilCorner * ((boardState[11] == 'o') + (boardState[18] == 'o') + (boardState[81] == 'o') + (boardState[88] == 'o'));
+
+		//value mobility
+		utility += utilMove * (games["othello"].constructChildren(boardState, 'x').filter(a => a != undefined).length - 
+								games["othello"].constructChildren(boardState, 'x').filter(a => a != undefined).length);
+
+		// #points
+		utility += utilCapture * (numXs - numOs);
+
+		//don't let utility be 0, because that indicates a stalemate
+		if (utility == 0) {
+			return 1;
+		}
+		return utility;
+	}
+}
+
+class TicTacToe extends Game {
+	constructor() {
+		super();
+		this.dims = [3, 3, 3];
+		this.thinkDist = 11;
+	}
+
+	constructChildren(boardState, player) {
+		var buffer1 = [];
+
+		//add all available squares
+		for (var c=0; c<boardState.length; c++) {
+			if (boardState[c] == boardEmpty) {
+				buffer1[c] = [boardChars[c], this.applyMove(boardState, c, player)];
+			}
+		}
+
+		return buffer1;
+	}
+}
+
+
+var games = {
+	"5-in-a-row": new FiveInARow(),
+	"connect-4": new ConnectFour(),
+	"tic-tac-toe": new TicTacToe(),
+	"order-and-chaos": new OrderAndChaos(),
+	"othello": new Othello(),
+}
+
+
+var games_desc = {
+	"5-in-a-row": [`Similar to tic-tac-toe, but 5 in a row`, `is required to win, and the board is 10 by 10.`],
+	"connect-4": [`Played on a 6 by 7 board.`, `Players drop tiles from the top of the board, and they fall to the lowest available spot.`, `The first player to make 4 in a row wins`],
+	"tic-tac-toe": [`Played with two players on a 3 by 3 board.`, `Each player tries to make 3 in a row to win the game.`, `One player plays only Xs, and one player plays only Os.`, `X goes first.`],
+	"order-and-chaos": [`Played on a 6 by 6 board.`, `One player is order, and the other is chaos.`, `Both players can place an X or O.`, `Order wins by making at least 5 in a row, and chaos wins by preventing that.`, `Order plays first. To play type either X or O and then the number of the space you want to play at.`],
+	"othello": [`Played on an 8 by 8 board.`, `Players take turns placing an X or O on the board.`, `If placing a tile creates a line of tiles bordered on each end by the same type, the tiles will switch sides.`, `The game ends when the entire board is filled.`]
+}
---
+++ X-O-games/main.js
@@ -0,0 +1,310 @@
+
+
+
+window.onload = setup;
+var asyncBuffer;
+
+var boardChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-=,.;'[]~!@#$%^&*()_+";
+var boardEmpty = ".";
+var board;
+
+var game_auto = false;
+var game_computerIs = undefined;
+var game_dat;
+var game_maxViewDepth = 12;
+var game_toPlay;
+
+var textDelayCurrent = 0;
+var textDelay = 2;
+var textLine = 0;
+var textBuffer = [];
+var textCommands = [];
+
+function setStandardTextCommands() {
+	textCommands = [
+		[`quit`, () => {text_setStandard(); stopAsyncActivity();}],
+		[`help`, (gameName) => {text_addHelp(gameName);}]
+
+];
+
+}
+
+function setup() {
+	setStandardTextCommands();
+	text_setStandard()
+	document.getElementById("terminal-input-true").addEventListener("keyup", acceptTextCommand, false);
+}
+
+
+function acceptTextCommand(a) {
+	//only submit commands on enter
+	if (a.key != "Enter") {
+		return;
+	}
+	var commandLocation = document.getElementById("terminal-input-true");
+	var command = commandLocation.value;
+
+	if (command == undefined) {
+		return;
+	}
+
+	//detect commands and clear box if accepted
+	var inputs = command.toUpperCase().split(" ");
+	for (var e=0; e<textCommands.length; e++) {
+		if (inputs[0] == textCommands[e][0].toUpperCase()) {
+			textCommands[e][1](...inputs.slice(1));
+			commandLocation.value = "";
+			return;
+		}
+	}
+}
+
+function displayText() {
+	textLine = 0;
+	document.getElementById("terminal-output").innerHTML = "";
+	displayTextRecursive();
+}
+
+function displayTextRecursive() {
+	//don't display if off the end
+	if (textLine > textBuffer.length-1) {
+		return;
+	}
+
+	//increment the text parity
+	textDelayCurrent += 1;
+
+	//display text
+	if (textDelayCurrent >= textDelay) {
+		textDelayCurrent = 0;
+		textLine += 1;
+
+		var strOutput = ``;
+		for (var h=0; h<textLine; h++) {
+			strOutput += textBuffer[h] + "<br>";
+		}
+
+		document.getElementById("terminal-output").innerHTML = strOutput;
+	}
+	window.requestAnimationFrame(displayTextRecursive);
+}
+
+function stopAsyncActivity() {
+	window.clearTimeout(asyncBuffer);
+	game_auto = false;
+}
+
+function text_addHelp(helpFor) {
+	//add at the top of the text buffer
+	textBuffer.splice(0, 0, `--------`);
+
+	var helpText = games_desc[helpFor];
+	helpText.forEach(h => {
+		textBuffer.splice(0, 0, h);
+	});
+}
+
+
+function text_runGame() {
+	game_toPlay = game_dat.incrementPlayer(game_toPlay);
+	setStandardTextCommands();
+
+	
+
+	//figure out if it's a computer or human turn
+	var turnIsRobot = game_auto || (game_toPlay == game_computerIs);
+	//I put the commands in a list so javascript arguments don't freak out on me
+	var commandList = game_dat.constructChildren(board, game_toPlay);
+	writeBoardToTerminal(turnIsRobot, commandList);
+
+	//is the game over? 
+	if (game_dat.gameIsOver(board)) {
+		var nowUtility = game_dat.utilityFor(board);
+		var dispText = "";
+		switch (true) {
+			case (nowUtility == game_dat.utilGoals[game_dat.pieces[0]]):
+				//p0 has won
+				dispText = `${game_dat.players[0]} has won.`;
+				break;
+			case (nowUtility == game_dat.utilGoals[game_dat.pieces[1]]):
+				//p1 has won
+				dispText = `${game_dat.players[1]} has won.`;
+				break;
+			case (nowUtility == 0):
+				//draw
+				dispText = `Stalemate.`;
+				break;
+			default:
+				dispText = `An Error has occurred in this game's utility function.`;
+		}
+		if (dispText != "") {
+			textBuffer.push("");
+			textBuffer.push(dispText);
+		}
+
+		//restart a new game if it's auto
+		if (game_auto) {
+			asyncBuffer = window.setTimeout(() => {
+				text_setPreparePlayers();
+				text_runGame();
+			}, 100);
+		} else {
+			//if it's manual, display the user's options
+			textBuffer.push(`You may [quit] or [restart].`);
+			textCommands.push(['restart', () => {text_setPreparePlayers();}]);
+		}
+
+		displayText();
+		return;
+	}
+	
+	if (!game_auto) {
+		textBuffer.push(`<br>${game_toPlay} to move.`);
+	}
+
+	//commands
+	if (turnIsRobot) {
+		//think hard
+		var moveTree = game_dat.generateMoveTree(board, game_dat.thinkDist, game_toPlay);
+		var moveUtilDist = undefined;
+		var moveBests = [];
+		//choose move based on utility of children
+		for (var k=0; k<moveTree.children.length; k++) {
+			if (moveUtilDist == undefined || Math.abs(moveTree.children[k].utility - game_dat.utilGoals[game_toPlay]) < moveUtilDist) {
+				moveUtilDist = Math.abs(moveTree.children[k].utility - game_dat.utilGoals[game_toPlay]);
+				moveBests = [moveTree.children[k].board];
+			} else if (Math.abs(moveTree.children[k].utility - game_dat.utilGoals[game_toPlay]) == moveUtilDist) {
+				//if it's equal, add it to the possible moves
+				moveBests.push(moveTree.children[k].board);
+			}
+		}
+
+		//choose best move
+		if (moveBests.length == 0) {
+			console.log(`something's gone wrong!!!`);
+		} else {
+			board = moveBests[Math.floor(Math.random() * (moveBests.length-0.01))];
+		}
+		asyncBuffer = window.setTimeout(() => {
+			text_runGame();
+		}, 5 + (20 * game_auto));
+	} else {
+		//allow input from player
+		commandList.forEach(c => {
+			textCommands.push([c[0], () => {board = c[1]; text_runGameBuffer();}])
+		});
+	}
+
+	//if it's a draw, display that.
+	displayText();
+}
+
+function text_runGameBuffer() {
+	writeBoardToTerminal(game_auto || (game_toPlay == game_computerIs), game_dat.constructChildren(board, game_toPlay));
+	displayText();
+	asyncBuffer = window.setTimeout(() => {
+		text_runGame();
+	}, 50);
+}
+
+function text_setCMDListing() {
+	//drawing
+	textBuffer = [
+		`COMMAND STRUCTURE: [command name] [arg1] [arg2] [arg3]`,
+		`Typed as: command name arg1 arg2 arg3`,
+		`[quit] - At any time, quit to the main space.`,
+		`[help] [game] - Get information about a game.`,
+	];
+
+	//commands
+	setStandardTextCommands();
+	textCommands = [...textCommands,
+		[],
+	]
+
+	displayText();
+}
+
+
+function text_setPreparePlayers(gameKey) {
+	if (gameKey != undefined) {
+		game_dat = games[gameKey];
+	}
+	game_maxViewDepth = game_dat.thinkDist;
+	game_toPlay = game_dat.players[1];
+	board = game_dat.createBoard();
+
+	//commands
+	setStandardTextCommands();
+	textCommands.push(['2', () => {game_computerIs = ""; text_runGame();}]);
+	textCommands.push(['1', () => {text_setPrepareRoles();}]);
+	textCommands.push(['0', () => {game_auto = true; text_runGame();}]);
+
+	//text
+	textBuffer = [`Enter number of human players.`, `[2] or [1]`];
+
+	displayText();
+}
+
+
+function text_setPrepareRoles() {
+	var players = game_dat.players;
+	//commands
+	setStandardTextCommands();
+	textCommands.push([players[0], () => {game_computerIs = players[1]; text_runGame();}]);
+	textCommands.push([players[1], () => {game_computerIs = players[0]; text_runGame();}]);
+
+	textBuffer = [`Would you like to be [${players[0]}] or [${players[1]}]?`];
+	displayText();
+}
+
+function text_setStandard() {
+	textBuffer = [
+		`Welcome.`,
+		`Typable commands will be in [square brackets].`,
+		`You may request a full [list] of commands.`,
+		`The intelligence for 5-in-a-row is currently incomplete.`,
+		`Please be prepared for flawed performance if playing with <2 players.`,
+		``,
+		`Please type a game selection:`,
+	];
+	
+	//set text + commands
+	setStandardTextCommands();
+	textCommands.push(["list", () => {text_setCMDListing();}]);
+	Object.keys(games).forEach(g => {
+		textBuffer.push(`[${g}]`);
+		textCommands.push([g, () => {text_setPreparePlayers(g);}])
+	});
+
+	displayText();
+}
+
+function writeBoardToTerminal(turnIsRobot, commandList) {
+	//first display current board state. I put breaks in so this is all done in one pass, no matter the draw speed.
+	var boardStr = "<br>";
+	var commandStr = "";
+	var commandLen = 0;
+	for (var a=0; a<commandList.length; a++) {
+		if (commandList[a] != undefined) {
+			commandLen = commandList[a][0].length;
+			a = commandList.length + 1;
+		}
+	}
+	for (var q=0; q<game_dat.dims[0] * game_dat.dims[1]; q+=game_dat.dims[0]) {
+		if (!turnIsRobot) {
+			for (var y=0; y<game_dat.dims[0]; y++) {
+				if (commandList[q+y] == undefined) {
+					commandStr += ` `.repeat(2 + commandLen);
+				} else {
+					commandStr += `[${commandList[q+y][0]}]`;
+				}
+			}
+		}
+		
+		boardStr += `${board.slice(q, q+game_dat.dims[0])}      ${commandStr}<br>`;
+		commandStr = "";
+	}
+
+	textBuffer = [boardStr];
+}
