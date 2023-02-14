3494c7c82f5cea23aeb378289e0987d71e63d49d
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2023-02-14 15:53:20 -0500

    Added bezierBezierIntersect and quadraticBounds

--- common/functions-bezier.js
+++ common/functions-bezier.js
@@ -6,17 +6,65 @@ requires:
 	functions-math.js
 
 
+bezierBezierIntersect(b1p0, b1c0, b1c1, b1p1, b2p0, b2c0, b2c1, b2p1, tolerance)
 lineBezierIntersect(linp1, linp2, bezp0, bezc0, bezc1, bezp1)
 pointBezierIntersect(p1, bezp0, bezc0, bezc1, bezp1, halfTolerance)
 inCurvedPoly(xyPoint, polyLines)
 bezierBounds(p0, c0, c1, p1)
 bezierPointFromT(p0, c0, c1, p1, t)
 quadraticPointFromT(p0, c, p1, t)
-bezierMinMax(p0, x1, y1, x2, y2, x3, y3)
 bezierSplit(p0, c0, c1, p1, percentage)
 
 
 */
+
+
+function bezierBezierIntersect(b1p0, b1c0, b1c1, b1p1, b2p0, b2c0, b2c1, b2p1, tolerance) {
+	var bounds1 = bezierBounds(b1p0, b1c0, b1c1, b1p1);
+	var bounds2 = bezierBounds(b2p0, b2c0, b2c1, b2p1);
+
+	//if the bounds don't intersect, the curves can't intersect
+	if (bounds1[0] > bounds2[2] + tolerance || bounds1[1] > bounds2[3] + tolerance || bounds1[2] < bounds2[0] - tolerance || bounds1[3] < bounds2[1] - tolerance) {
+		return false;
+	}
+	
+	//if the boxes are too small, count the curves as intersecting
+	var small1 = (bounds1[2] - bounds1[0] < tolerance && bounds1[3] - bounds1[1] < tolerance);
+	var small2 = (bounds2[2] - bounds2[0] < tolerance && bounds2[3] - bounds2[1] < tolerance);
+	
+	//both of them are small enough to count as touching
+	if (small1 && small2) {
+		return true;
+	}
+	
+
+	//it's actually very difficult to tell if these curves intersect so we're gonna split at the center and try this again
+	var children1;
+	var children2;
+	if (!small1 && !small2) {
+		children1 = bezierSplit(b1p0, b1c0, b1c1, b1p1, 0.5);
+		children2 = bezierSplit(b2p0, b2c0, b2c1, b2p1, 0.5);
+
+		//check all combinations of intersecting children
+		return (bezierBezierIntersect(...children1[0], ...children2[0], tolerance) 
+			|| bezierBezierIntersect(...children1[0], ...children2[1], tolerance) 
+			|| bezierBezierIntersect(...children1[1], ...children2[0], tolerance) 
+			|| bezierBezierIntersect(...children1[1], ...children2[1], tolerance));
+	}
+
+	//don't split a curve that's already small enough
+	if (small1) {
+		children2 = bezierSplit(b2p0, b2c0, b2c1, b2p1, 0.5);
+		return (bezierBezierIntersect(b1p0, b1c0, b1c1, b1p1, ...children2[0], tolerance) || bezierBezierIntersect(b1p0, b1c0, b1c1, b1p1, ...children2[1], tolerance));
+	}
+
+	//only 2 is small
+	children1 = bezierSplit(b1p0, b1c0, b1c1, b1p1, 0.5);
+	return (bezierBezierIntersect(...children1[0], b2p0, b2c0, b2c1, b2p1, tolerance) || bezierBezierIntersect(...children1[1], b2p0, b2c0, b2c1, b2p1, tolerance));
+}
+
+
+
 //Says whether a line segment and a bezier curve intersect
 function lineBezierIntersect(linp1, linp2, bezp0, bezc0, bezc1, bezp1) {
 	var tolerance = 0.04;
@@ -90,10 +138,9 @@ function inCurvedPoly(xyPoint, polyLines) {
 
 
 //the bounds of a bezier curve can be found either at the start / end, or where the derivative of x/y is 0 (because that's where turns happen)
-//
-
+//returns [min x, min y, max x, max y]
 function bezierBounds(p0, c0, c1, p1) {
-	return bezierMinMax(p0, c0[0], c0[1], c1[0], c1[1], p1[0], p1[1]);
+	return bezierMinMax(p0, c0, c1, p1);
 }
 
 //given the 4 points and a t value, gives the point on the bezier curve that corresponds to that t-value
@@ -110,8 +157,32 @@ function coefficientsForBezier(x0, c0, c1, x1) {
 	return [x1 - x0 + 3*c0 - 3*c1, 3*c1 + 3*x0 - 6*c0, 3*c0 - 3*x0, x0];
 }
 
+//similar algorithm to bezierBounds - test endpoints and spots where the derivative is 0
 function quadraticBounds(p0, c, p1) {
+	var xValues = [];
+	var yValues = [];
+	var a, b, t;
 
+	//start and end points
+	xValues.push(p0[0], p1[0]);
+	yValues.push(p0[1], p1[1]);
+
+	//check x and then y slope
+	for (var i=0; i<2; i++) {
+		a = p0[i] - 2 * c[i] + p1[i];
+		b = -2 * p0[i] + 2 * c[i];
+		t = -b / (2 * a);
+		//make sure t is actually part of the curve
+		if (t > 0 && t < 1) {
+			((i == 0) ? xValues : yValues).push(quadraticPointFromT(p0, c, p1, t)[i]);
+		}
+	}
+
+
+	return [
+		Math.min(...xValues), Math.min(...yValues),
+		Math.max(...xValues), Math.max(...yValues)
+	];
 }
 
 //given 3 points of a quadratic curve / t value, gives the point on the curve corresponding to the t valuez
@@ -126,19 +197,15 @@ function quadraticPointFromT(p0, c, p1, t) {
 The curve changes direction when the derivative of the x or y components hit zero, so this runs the quadratic formula twice
 (once for x and once for y)
 */
-function bezierMinMax(p0, x1, y1, x2, y2, x3, y3) {
+//TODO: could this be optimized? It seems to check both the x and y of a 0-slope point, when only one dimension should be necessary
+function bezierMinMax(p0, c0, c1, p1) {
 	var tvalues = [], xvalues = [], yvalues = [],
 		a, b, c, t, t1, t2, b2ac, sqrtb2ac;
 	for (var i=0; i<2; i++) {
-		if (i == 0) {
-			b = 6 * p0[0] - 12 * x1 + 6 * x2;
-			a = -3 * p0[0] + 9 * x1 - 9 * x2 + 3 * x3;
-			c = 3 * x1 - 3 * p0[0];
-		} else {
-			b = 6 * p0[1] - 12 * y1 + 6 * y2;
-			a = -3 * p0[1] + 9 * y1 - 9 * y2 + 3 * y3;
-			c = 3 * y1 - 3 * p0[1];
-		}
+		b = 6 * p0[i] - 12 * c0[i] + 6 * c1[i];
+		a = -3 * p0[i] + 9 * c0[i] - 9 * c1[i] + 3 * p1[i];
+		c = 3 * c0[i] - 3 * p0[i];
+
 		if (Math.abs(a) < 1e-12) {
 			if (Math.abs(b) < 1e-12) {
 				continue;
@@ -174,16 +241,16 @@ function bezierMinMax(p0, x1, y1, x2, y2, x3, y3) {
 	while (j--) {
 		t = tvalues[j];
 		mt = 1 - t;
-		xvalues[j] = (mt * mt * mt * p0[0]) + (3 * mt * mt * t * x1) + (3 * mt * t * t * x2) + (t * t * t * x3);
-		yvalues[j] = (mt * mt * mt * p0[1]) + (3 * mt * mt * t * y1) + (3 * mt * t * t * y2) + (t * t * t * y3);
+		xvalues[j] = (mt * mt * mt * p0[0]) + (3 * mt * mt * t * c0[0]) + (3 * mt * t * t * c1[0]) + (t * t * t * p1[0]);
+		yvalues[j] = (mt * mt * mt * p0[1]) + (3 * mt * mt * t * c0[1]) + (3 * mt * t * t * c1[1]) + (t * t * t * p1[1]);
 	}
 
-	xvalues.push(p0[0],x3);
-	yvalues.push(p0[1],y3);
+	xvalues.push(p0[0],p1[0]);
+	yvalues.push(p0[1],p1[1]);
 
 	return [
-		[Math.min(...xvalues), Math.min(...yvalues)],
-		[Math.max(...xvalues), Math.max(...yvalues)]
+		Math.min(...xvalues), Math.min(...yvalues),
+		Math.max(...xvalues), Math.max(...yvalues)
 	];
 }
 
