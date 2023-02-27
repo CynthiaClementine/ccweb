c85af8833db921efa3cb034b710575ecddbd39b8
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2023-02-27 14:07:29 -0500

    Added file

---
+++ plains/conversations.js
@@ -0,0 +1,50 @@
+var data_conversations = {
+	"solilo-1": [
+		`Amazing! This truly is one of the places I have been.`,
+		`So many sights to see, so many things to do, so many entities to greet. 
+		Perhaps that gatekeeper knows a thing or two. Or perhaps not.`
+	],
+
+	"audioLog-1": [
+		`And so, finding no consolation outside, he turned his attention inwards,
+		and began construction on a most magnificent project.
+		This shining temple was to stand as a testament to the progress people as a whole had made.`
+	],
+
+	"gatekeeper-1": [
+		`@...Hello?`,
+		`Hark! A visitor. It has been a long time since anyone has appeared in these outskirts.`,
+		`You want to know what's going on? Where things are?`,
+		`...`,
+		`I must confess, I do not know.`,
+		`Surprisingly, you don't gain much information by standing in one spot for thousands of years.`,
+		`But I can tell you about the star patterns if you so enquire... 
+		the gradual changes in shine and luster revealing a whole other universe out there..`,
+		`@I think I'll pass.`
+	],
+	"gatekeeper-2": [
+		`200 years ago Osborne came to this here place.
+		He's been innovating ever since.`,
+		`Although.. between you and me, innovating is just his term. 
+		I'm not sure he's actually doing anything.`,
+		`What kind of farmer loses his Flux? I saw one way up on the catwalks once.`,
+		`@I'll keep that in mind`
+	],
+
+
+
+
+	"osborne-1": [
+		`H\t\tI\t\t\t!\t\t\t\t`,
+		`I am a tender of these flux fields. But...
+		I've lost my Flux.`,
+		`Can you. Help get them back?`
+	],
+	"osborne-2": [
+		`Please.. gather all of my Flux`
+	],
+	"osborne-3": [
+		`Amazing! Thank you so much. Now these flux fields will be bountiful once more.`,
+		`I will never\t forget this, not even when the sun covers the earth in its plasma.`
+	]
+}
