107972ccb01da9963eb5700930c01cde8ac19f62
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2023-05-20 23:58:30 -0400

    added functions

--- common/functions-2dVector.js
+++ common/functions-2dVector.js
@@ -19,3 +19,11 @@ function d2_normalize(v) {
 	var len = Math.sqrt(v[0] * v[0] + v[1] * v[1]);
 	return [v[0] / len, v[1] / len];
 }
+
+function d2_distance(v1, v2) {
+	return Math.sqrt((v1[0] - v2[0]) ** 2 + (v1[1] - v2[1]) ** 2);
+}
+
+function d2_distSquared(v1, v2) {
+	return ((v1[0] - v2[0]) ** 2 + (v1[1] - v2[1]) ** 2);
+}
