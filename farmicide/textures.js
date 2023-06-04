e2d8aa3b75db3e429679472298fea5a4091888b1
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2023-06-04 19:37:09 -0400

    Created

--- farmicide/textures.js
+++ farmicide/textures.js
@@ -1,2 +1,5 @@
 
+var image_corn = getImage(`img/corn.png`);
 var image_ground = getImage(`img/MapWDirtWOGrid.png`, true);
+var image_player = getImage(`img/Chibi.png`, true);
+var texture_vendors = new Texture(getImage(`img/vendors.png`, true), 320, 1e1001, [1, 1], [0.5, 1], [[0, 0], [1, 0], [2, 0], [3, 0], [0, 1]], false);
