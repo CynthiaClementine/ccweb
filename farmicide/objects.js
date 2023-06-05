e515ef7e021f2e7553ca6ad0d3a244cbea7fd37f
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2023-06-05 15:57:48 -0400

    Main working

--- farmicide/objects.js
+++ farmicide/objects.js
@@ -6,6 +6,8 @@ class Entity {
 		this.owner = owner;
 		
 		this.r = 0.5;
+		this.health = 1e1001;
+		this.healthMax = this.health;
 		this.interactable = false;
 	}
 
@@ -13,96 +15,205 @@ class Entity {
 		//repel players
 		circleRepel(player1, this.x, this.y, this.r);
 		circleRepel(player2, this.x, this.y, this.r);
+
+		//increase age, if has it
+		if (this.age != undefined) {
+			this.age += 1;
+		}
+
+		if (this.health <= 0) {
+			this.DELETE = true;
+		}
 	}
 
 	draw() {
-
+		var cord = spaceToScreen(this.x, this.y);
+		if (this.health < this.healthMax) {
+			drawCircularMeter(cord[0], cord[1], game_tileSize * 0.5, game_tileSize * 0.375, color_health, this.health / this.healthMax);
+		}
 	}
 }
-
 
 
 class Beacon extends Entity {
 	constructor(x, y, owner) {
 		super(x, y, owner);
+		this.health = 4;
+		this.healthMax = 4;
 		this.claimR = 8;
+	}
+
+	draw() {
+		super.draw();
+		var cord = spaceToScreen(this.x, this.y);
+		ctx.drawImage(image_beacon, cord[0] - game_tileSize * 0.5, cord[1] - game_tileSize, game_tileSize, game_tileSize);
 	}
 }
 
 
 class Bullet extends Entity {
-	constructor(x, y, a, v, owner) {
+	constructor(x, y, a, v, owner, producer) {
 		super(x, y, owner);
+		//bullets have a producer tag so entities that are hit know where it originated from
+		this.producer = producer;
+		this.r = 0.3;
+		this.a = a;
+		this.v = v;
 		[this.dx, this.dy] = polToXY(0, 0, a, v);
 	}
 
-	tick () {
+	tick() {
 		//loop through the entities
-		//if close to one that doesn't have the same owner as self, damage them
+		var harmable = entities.filter(e => e != this && e != this.producer && e.health >= 0 && e.owner != (this.owner ?? "uniqueStr"));
+		//walls are a special case - ones of the same owner can never be hit, even if in friendly fire mode
+		harmable = harmable.filter(w => w.constructor.name != "Rock" || w.owner != this.producer.owner);
+
+		//if close to one, damage them
+		for (var h=0; h<harmable.length; h++) {
+			if (Math.hypot(this.x - harmable[h].x, this.y - harmable[h].y) < this.v + this.r + harmable[h].r * 0.25) {
+				harmable[h].health -= 1;
+				if (harmable[h].hitAlert) {
+					harmable[h].hitAlert(this.producer);
+				}
+				this.DELETE = true;
+				h = harmable.length + 1;
+			}
+		}
+		//move
+		this.x += this.dx;
+		this.y += this.dy;
+
+		//if off the screen leave
+		var coords = spaceToScreen(this.x, this.y);
+		if (Math.abs(coords[0]) > canvas.width / 2 || Math.abs(coords[1]) > canvas.height / 2) {
+			this.DELETE = true;
+		}
 	}
 
 	draw() {
-
+		texture_potato.draw(...spaceToScreen(this.x, this.y), this.a + Math.PI / 2, game_tileSize * this.r * 2);
 	}
 }
 
+class Chicken extends Entity {
 
-class Vendor extends Entity {
-	constructor(x, y, itemID) {
-		super(x, y, undefined);
-		this.r = 1;
-		this.itemID = itemID;
-		this.cost = entity_data[itemID].price;
+}
 
-		this.interactable = true;
-		this.interactText = `Buy ${itemID} (${entity_data[itemID].price}¢)`;
+class Coin extends Entity {
+	constructor(x, y, owner) {
+		super(x, y, owner);
+		this.r = 0.25;
+		this.health = undefined;
+
+		this.dx = 0;
+		this.dy = 0;
+		this.friction = 0.95;
+		this.force = 0.001;
 	}
 
-	interact(player) {
-		//can't sell to the player if they don't have enough money
-		if (player.money < this.cost) {
+	tick() {
+		//be absorbed by players if close enough
+		//terrible code but L + ratio + it doesn't matter that much
+		if (Math.hypot(this.x - player1.x, this.y - player1.y) < this.r + player1.r) {
+			this.DELETE = true;
+			player1.money += 1;
 			return;
 		}
-		
-		//can't give player more than one item
-		if (player.bag[0] != undefined && player.bag[0] != this.itemID) {
+		if (Math.hypot(this.x - player2.x, this.y - player2.y) < this.r + player2.r) {
+			this.DELETE = true;
+			player2.money += 1;
 			return;
 		}
 
-		//finally give the player the item
-		player.money -= this.cost;
-		player.bag[0] = this.itemID;
-		player.bag[1] += 1;
+		//move towards owner
+		if (this.owner == undefined) {
+			return;
+		}
+		var ownVec = [this.owner.x - this.x, this.owner.y - this.y];
+		var ownDist = Math.hypot(...ownVec);
+		ownVec[0] /= ownDist;
+		ownVec[1] /= ownDist;
+		this.dx += ownVec[0] * this.force / ownDist;
+		this.dy += ownVec[1] * this.force / ownDist;
+
+		this.dx *= this.friction;
+		this.dy *= this.friction;
+
+		this.x += this.dx;
+		this.y += this.dy;
 	}
 
 	draw() {
-		texture_vendors.frame = entity_data[this.itemID].vendorID;
-		texture_vendors.draw(...spaceToScreen(this.x, this.y), 0, game_tileSize * 2);
+		//yelow cirlce
+		var cd = spaceToScreen(this.x, this.y);
+		ctx.fillStyle = color_coin;
+		ctx.beginPath();
+		ctx.ellipse(cd[0], cd[1], game_tileSize * 0.2, game_tileSize * 0.2, 0, 0, Math.PI * 2);
+		ctx.fill();
 	}
 }
 
 class Crop extends Entity {
 	constructor(x, y, owner) {
 		super(x, y, owner);
-		this.claimR = 2;
+		this.claimR = 1.5;
+		this.scatterR = 1.85;
+		this.health = 3;
+		this.healthMax = 3;
 		this.age = 0;
-		this.ageMax = entity_data["corn"].maxAge;
+		this.ageMax = 550;
+		this.ageMaxTrue = 1200;
+		this.harvestTime = 300;
+		this.overripeTime = 200;
 		this.texture = new Texture(image_corn, 120, this.ageMax / 4, [2, 2], [1, 1.75], [[0, 0], [2, 0], [4, 0], [6, 0]], false);
+		this.interactText = `Growing...`;
+	}
+
+	interact() {
+		//don't do anything while the crop is still growing
+		if (this.age < this.ageMax + this.harvestTime) {
+			return;
+		}
+
+		//if it's ripe, harvest
+		this.harvest();
 	}
 
 	harvest() {
+		//scatter coin (corn?) throughout the land
+		var coinNum = Math.floor(10 + getPercentage(this.ageMax + this.harvestTime, this.ageMax + this.harvestTime + this.overripeTime, this.age));
+		var r, theta;
+		for (var n=0; n<coinNum; n++) {
+			r = randomBounded(0, this.scatterR / 2) + randomBounded(0, this.scatterR / 2);
+			theta = randomBounded(0, Math.PI * 2);
+			entities.push(new Coin(...polToXY(this.x, this.y, theta, r), this.owner));
+		}
 
+		//reset age
+		this.interactText = `Growing...`;
+		this.age = this.ageMax;
 	}
 
 	tick() {
 		super.tick();
-		this.age += 1;
+		if (this.age >= this.ageMax + this.harvestTime) {
+			this.interactText = `Harvest!`;
+		}
+		if (this.age > this.ageMaxTrue) {
+			this.age = this.ageMaxTrue;
+		}
 	}
 
 	draw() {
+		super.draw();
+
+		//harvest timer
 		//draw different stage based on age
 		var coords = spaceToScreen(this.x, this.y);
-		this.texture.draw(coords[0], coords[1], 0, game_tileSize);
+		if (this.age > this.ageMax) {
+			drawCircularMeter(coords[0], coords[1], game_tileSize * 0.3, game_tileSize * 0.2, color_cropWindup, (this.age - this.ageMax) / (this.harvestTime));
+		}
+		this.texture.draw(coords[0], coords[1] + game_tileSize * 0.2, 0, game_tileSize);
 	}
 }
 
@@ -128,9 +239,9 @@ class Player {
 
 		this.bag = [undefined, 0];
 		this.money = player_moneyStart;
-		this.health = 100;
-		this.healthMax = 100;
-		this.regenRate = 0.02;
+		this.health = 10;
+		this.healthMax = 10;
+		this.regenRate = 0.002;
 	}
 
 	tick() {
@@ -180,7 +291,7 @@ class Player {
 		}
 
 		//health
-		if (data_persistent.regen && this.health > 0) {
+		if (data_persistent.regen && this.health > 0 && this.health < this.healthMax) {
 			this.health += Math.abs(Math.sin(timer / 100) * this.regenRate);
 		}
 	}
@@ -206,11 +317,11 @@ class Player {
 
 	//finds the closest interactable entity
 	findInteractable() {
-		var distCap = 2.5 ** 2;
+		var distCap = 2.5;
 		var bestEntity, dist;
 		for (var e=0; e<entities.length; e++) {
-			if (entities[e].constructor.name != "Player") {
-				dist = (entities[e].x - this.x) ** 2 + (entities[e].y - this.y) ** 2;
+			if (this.couldInteract(entities[e])) {
+				dist = Math.hypot(entities[e].x - this.x, entities[e].y - this.y);
 				if (dist < distCap) {
 					bestEntity = entities[e];
 					distCap = dist;
@@ -220,9 +331,17 @@ class Player {
 		return bestEntity;
 	}
 
+	couldInteract(entity) {
+		return (entity.constructor.name != "Player" && entity.owner != ((player1 == this) ? player2 : player1) && entity.interact);
+	}
+
 	interact() {
 		var interObj = this.findInteractable();
 		if (interObj == undefined) {
+			//if there's something in our bag, place it
+			if (this.bag[0] != undefined) {
+				this.placeEntityAt(this.x, this.y);
+			}
 			return;
 		}
 		interObj.interact(this);
@@ -230,28 +349,176 @@ class Player {
 
 	draw() {
 		var selfCoords = spaceToScreen(this.x, this.y);
-		var startCoords = spaceToScreen(this.x - 0.85, this.y - 2);
-		var endCoords = spaceToScreen(this.x + 0.85, this.y);
+		var startCoords = spaceToScreen(this.x - 0.75, this.y - 2);
+		var endCoords = spaceToScreen(this.x + 0.95, this.y);
+		
+		drawCircularMeter(selfCoords[0], selfCoords[1], game_tileSize * 0.5, game_tileSize * 0.375, color_health, this.health / this.healthMax);
 
 		//draw box
 		var textSize = game_tileSize / 3;
 		ctx.fillStyle = color_boxBG2;
-		ctx.fillRect(selfCoords[0] - game_tileSize * 1.05, selfCoords[1] - game_tileSize * 4.05, game_tileSize * 2.1, game_tileSize * 1.1);
+		ctx.fillRect(selfCoords[0] - game_tileSize * 1.35, selfCoords[1] - game_tileSize * 3.05, game_tileSize * 2.7, game_tileSize * 1.1);
 		ctx.fillStyle = color_boxBG;
-		ctx.fillRect(selfCoords[0] - game_tileSize, selfCoords[1] - game_tileSize * 4, game_tileSize * 2, game_tileSize);
-		ctx.font = `${textSize}px Lato`;
+		ctx.fillRect(selfCoords[0] - game_tileSize * 1.3, selfCoords[1] - game_tileSize * 3, game_tileSize * 2.6, game_tileSize);
 		var text = [
 			(this.findInteractable() ?? {interactText: ``}).interactText,
 			(this.bag[0] == undefined) ? `Empty` : `${this.bag[0]} x${this.bag[1]}`,
 			this.money + `¢`
 		];
 		ctx.fillStyle = color_textPlayer;
+		ctx.font = `${textSize}px Lato`;
+		ctx.textAlign = "center";
 
 		for (var t=0; t<text.length; t++) {
-			
+			ctx.fillText(text[t], selfCoords[0], selfCoords[1] - game_tileSize * 3 + textSize * (0.5 + t));
 		}
 
 		ctx.drawImage(image_player, 0, 0, image_player.width, image_player.height, ...startCoords, endCoords[0] - startCoords[0], endCoords[1] - startCoords[1]);
+	}
+}
+
+
+class Rock extends Entity {
+	constructor(x, y, owner) {
+		super(x, y, owner);
+		this.r = 1;
+		this.claimR = 0.5;
+		this.health = 5;
+		this.healthMax = 5;
+	}
+
+	draw() {
+		super.draw();
+		var cord = spaceToScreen(this.x, this.y);
+		ctx.drawImage(image_rock, cord[0] - game_tileSize * 0.5, cord[1] - game_tileSize, game_tileSize, game_tileSize);
+	}
+}
+
+
+class Turret extends Entity {
+	constructor(x, y, owner) {
+		super(x, y, owner);
+		this.claimR = 1;
+		this.r = 0.75;
+		this.a = 0;
+		this.target;
+		this.range = 24;
+		this.health = 5;
+		this.healthMax = 5;
+		this.fireCooldown = 180;
+		this.age = 0;
+
+		this.bodyYOff = 0.8;
+		this.priorityList = [
+			"Player",
+			"Turret",
+			"Beacon",
+			"Crop",
+			"Chicken",
+			"Rock",
+		];
+		this.distancePriority = 5;
+		this.entityPriority = 10;
+		this.entityPriorityMult = 1.5;
+	}
+
+	retarget() {
+		var scoreBest = 0;
+		var entityBest;
+		var trueEnts = entities.filter(e => e != this && e != this.owner && e.owner != (this.owner ?? "un") && this.priorityList.includes(e.constructor.name));
+		var score, dist;
+
+		trueEnts.forEach(e => {
+			dist =  Math.hypot(e.x - this.x, e.y - this.y);
+			if (dist > this.range) {
+				return;
+			}
+			score = (this.entityPriority / (this.entityPriorityMult ** this.priorityList.indexOf(e.constructor.name))) * this.distancePriority / dist;
+			if (score > scoreBest) {
+				scoreBest = score;
+				entityBest = e;
+			}
+		});
+
+		this.target = entityBest;
+	}
+
+	fire() {
+		if (this.target == undefined) {
+			return;
+		}
+
+		//create potato
+		var potatoPos = polToXY(this.x, this.y - this.bodyYOff, this.a, 1);
+		entities.push(new Bullet(...potatoPos, this.a, bullet_velocity, data_persistent.friendlyFire ? undefined : this.owner, this));
+	}
+
+	hitAlert(entity) {
+		//target that entity
+		this.target = entity;
+	}
+
+	tick() {
+		super.tick();
+		
+		//retargeting
+		if (!entities.includes(this.target) || this.age % 200 == 0) {
+			this.target = undefined;
+		}
+		if (this.target == undefined || Math.hypot(this.target.x - this.x, this.target.y - this.y) > this.range) {
+			this.retarget();
+		}
+
+		if (this.target != undefined) {
+			this.a = Math.PI + Math.atan2((this.y - this.bodyYOff) - this.target.y, this.x - this.target.x);
+		}
+
+		//firing
+		if (this.age % this.fireCooldown == 0) {
+			this.fire();
+		}
+	}
+
+	draw() {
+		super.draw();
+		var cd = spaceToScreen(this.x, this.y);
+		texture_turretStand.draw(cd[0], cd[1], 0, game_tileSize);
+		texture_turretArm.draw(cd[0], cd[1] - this.bodyYOff * game_tileSize, this.a, game_tileSize / 4);
+	}
+}
+
+
+class Vendor extends Entity {
+	constructor(x, y, itemID) {
+		super(x, y, undefined);
+		this.r = 1;
+		this.itemID = itemID;
+		this.cost = entity_data[itemID].price;
+
+		this.interactable = true;
+		this.interactText = `Buy ${itemID} (${entity_data[itemID].price}¢)`;
+	}
+
+	interact(player) {
+		//can't sell to the player if they don't have enough money
+		if (player.money < this.cost) {
+			return;
+		}
+		
+		//can't give player more than one item
+		if (player.bag[0] != undefined && player.bag[0] != this.itemID) {
+			return;
+		}
+
+		//finally give the player the item
+		player.money -= this.cost;
+		player.bag[0] = this.itemID;
+		player.bag[1] += 1;
+	}
+
+	draw() {
+		texture_vendors.frame = entity_data[this.itemID].vendorID;
+		texture_vendors.draw(...spaceToScreen(this.x, this.y), 0, game_tileSize * 2);
 	}
 }
 
