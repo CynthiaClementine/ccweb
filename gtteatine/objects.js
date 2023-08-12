e0b65c88a309efc0334515e78f4d38cf7c1845dc
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2023-08-12 16:38:47 -0400

    Changed player radius to fit on large bridges

--- gtteatine/objects.js
+++ gtteatine/objects.js
@@ -12,6 +12,9 @@ class Player {
 		this.ax = 30;
 		this.ay = 0.7;
 
+		this.rx = 0.25;
+		this.ry = 0.55;
+
 		this.dir = 0;
 
 		this.dims = [0.08, 0.1];
@@ -33,6 +36,7 @@ class Player {
 
 	tick(dt) {
 		this.pickPowerups();
+		var activeAY = this.ay * (gravTime ? 0.7 : 1);
 
 		//velocity
 		this.dx = clamp(this.dx + this.dir * this.ax * dt, -this.strafe, this.strafe);
@@ -40,14 +44,14 @@ class Player {
 			this.dx *= this.friction;
 		}
 
-		this.dy -= this.ay * dt * 0.5;
+		this.dy -= activeAY * dt * 0.5;
 
 		//coordinates
 		var bridgeWidth = tileWidth * (sliceTiles - 0.25) * 0.5;
 		
 		this.y += this.dy;
 		//dy is split in half so it's more precisely integrated
-		this.dy -= this.ay * dt * 0.5;
+		this.dy -= activeAY * dt * 0.5;
 		if (!this.dead) {
 			this.x = clamp(this.x + this.dx * dt, -bridgeWidth, bridgeWidth);
 			this.z += calcZSpeed(this.z) * dt;
@@ -71,7 +75,6 @@ class Player {
 	heatWater() {
 		var bc = spaceToBridge(this.x, this.z);
 		bridge[bc[1]][bc[0]] = 10;
-		console.log(bc[1], bc[0]);
 		this.hotFeet = false;
 	}
 
@@ -101,12 +104,20 @@ class Player {
 
 		//low-grav
 		if (t == 4) {
-
+			if (gravTime == 0) {
+				gravTime = gravTimeMax;
+			} else if (gravTime > gravTimeMax * 0.25 && gravTime < gravTimeMax * 0.75) {
+				gravTime = gravTimeMax * 0.75;
+			} else if (gravTime < gravTimeMax * 0.25) {
+				gravTime = gravTimeMax - gravTime;
+			}
 		}
 
 		//boat
 		if (t == 5) {
-
+			player = new Player_Boat(this.x, this.z, this.hotFeet);
+			player.dx = this.dx;
+			player.dir = this.dir;
 		}
 
 		//obsidian
@@ -116,17 +127,17 @@ class Player {
 
 		//pick it up
 		bridge[bc[1]][bc[0]] = 1;
-
 	}
 
 	draw() {
-		var coords = spaceToScreen(this.x, this.y + calcHeight(playerTrueZ), playerTrueZ);
-		coords[1] -= canvas.height / 10;
+		var coordsLow = spaceToScreen(this.x, this.y + calcHeight(playerTrueZ), playerTrueZ);
+		var coordsX = spaceToScreen(this.x + this.rx, this.y + calcHeight(playerTrueZ) + this.ry, playerTrueZ);
+		var coords = spaceToScreen(this.x, this.y + calcHeight(playerTrueZ) + this.ry, playerTrueZ);
 		var base = spaceToScreen(this.x, calcHeight(playerTrueZ), playerTrueZ)[1] - (canvas.height / 10);
 
-		var ry = canvas.height / 10;
-		var rx = canvas.height / 15;
-		var rs = canvas.height / 100;
+		var ry = coordsLow[1] - coords[1];
+		var rx = coordsX[0] - coords[0];
+		var rs = canvas.height / 200;
 
 		//clipping is slow but also consider that I'm lazy
 		if (this.dead) {
@@ -135,8 +146,7 @@ class Player {
 			ctx.rect(0, 0, canvas.width, base + ry * 1.1);
 			ctx.clip();
 		}
-		ctx.fillStyle = "#F8F";
-		drawEllipse(coords[0], coords[1], rx, ry, "#F8F");
+		drawEllipse(coords[0], coords[1], rx, ry, color_player);
 
 		if (this.hotFeet) {
 			for (var k=0; k<10; k++) {
@@ -154,13 +164,103 @@ class Player_Boat extends Player {
 	constructor(x, z, hasHotFeet) {
 		super(x, z, hasHotFeet);
 
+		this.speedStoreL = this.speedMax;
+		this.speedStoreS = this.speedMin;
+
 		this.sinkAmount = 0.15;
 		this.boostWater = 1.2;
-		this.boostLand = 0.5;
+		this.boostLand = 0.7;
+
+		this.rx = 0.2;
+		this.ry = 0.5;
+
+		this.healthMax = 2.9;
+		this.health = 2.9;
+	}
+
+	tick(dt) {
+		this.speedMax = this.speedStoreL * (this.onGround() ? this.boostLand : this.boostWater);
+		this.speedMin = this.speedStoreS * (this.onGround() ? this.boostLand : this.boostWater);
+
+
+		super.tick(dt);
+		if (this.dead) {
+			this.dead = false;
+		}
+		if (this.y < -this.sinkAmount) {
+			this.y = -this.sinkAmount;
+			this.dy = 0;
+		}
+		if (this.onGround()) {
+			this.health -= dt;
+			if (this.health < 0) {
+				player = new Player(this.x, this.z, this.hotFeet);
+				player.dx = this.dx;
+				player.dir = this.dir;
+			}
+		}
 	}
 
 	draw() {
-		var coords = spaceToScreen(this.x, calcHeight(playerTrueZ) - this.sinkAmount * (this.onGround() > 0), playerTrueZ);
+		var centerPos = [this.x, calcHeight(playerTrueZ) + this.sinkAmount * (this.onGround() > 0), playerTrueZ];
+
+		this.drawFace(boat_faces[0], centerPos, color_boat);
+		if (this.x <= 0) {
+			this.drawFace(boat_faces[1], centerPos, color_boatLight);
+		}
+		if (this.x >= 0) {
+			this.drawFace(boat_faces[2], centerPos, color_boatLight);
+		}
+		this.drawFace(boat_faces[3], centerPos, color_boatLight);
+		this.drawFace(boat_faces[4], centerPos, color_boatLight);
+
+		//player
+		super.draw();
+
+		if (this.x > 0) {
+			this.drawFace(boat_faces[1], centerPos, color_boatLight);
+		}
+		if (this.x < 0) {
+			this.drawFace(boat_faces[2], centerPos, color_boatLight);
+		}
+		this.drawFace(boat_faces[5], centerPos, color_boatLight);
+		this.drawFace(boat_faces[6], centerPos, color_boatLight);
+
+		//draw health bar
+		var perc = this.health / this.healthMax;
+		var barInd = perc * color_bar.length;
+		centerPos[1] += this.ry * 2.2;
+		var barPos = spaceToScreen(...centerPos);
+		barPos[0] -= canvas.width * boat_barWidth * perc * 0.5;
+		barPos[1] -= canvas.height * (boat_barHeight * 0.5);
+
+		ctx.fillStyle = color_bar[Math.floor(barInd)];
+		ctx.fillRect(barPos[0], barPos[1], canvas.width * boat_barWidth * perc, canvas.height * boat_barHeight);
+		ctx.globalAlpha = barInd % 1;
+		ctx.fillStyle = color_bar[Math.ceil(barInd)];
+		ctx.fillRect(barPos[0], barPos[1], canvas.width * boat_barWidth * perc, canvas.height * boat_barHeight);
+		ctx.globalAlpha = 1;
+	}
+
+	drawFace(faceArr, centerPos, color) {
+		var ptsArr = faceArr.map(a => [...boat_verts[a]]);
+		for (var p=0; p<ptsArr.length; p++) {
+			[ptsArr[p][0], ptsArr[p][2]] = rotate(ptsArr[p][0], ptsArr[p][2], -0.4 * this.dx / this.strafe);
+		}
+		ptsArr = ptsArr.map(p => [p[0] * boat_scaling + centerPos[0], p[1] * boat_scaling + centerPos[1], p[2] * boat_scaling + centerPos[2]])
+					.map(c => spaceToScreen(c[0], c[1], c[2]));
+		ctx.strokeStyle = color;
+		ctx.fillStyle = color;
+		ctx.beginPath();
+		ctx.moveTo(ptsArr[0][0], ptsArr[0][1]);
+		for (var x=1; x<ptsArr.length; x++) {
+			ctx.lineTo(ptsArr[x][0], ptsArr[x][1]);
+		}
+		ctx.fill();
+		ctx.stroke();
+	}
+
+	heatWater() {
 
 	}
 }
