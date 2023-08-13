3a1236c05853aa15b9ae04f5364aa86c0b2feb58
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2023-08-13 17:00:00 -0400

    added hue conversions

--- common/functions-color.js
+++ common/functions-color.js
@@ -4,7 +4,7 @@ a set of functions dealing with manipulating color. Generally in hex form becaus
 
 INDEX
 linterp(a, b, t)
-cBreakdown(hexRGBA)
+cBreakdownRGBA(rgbaString)
 cBrightness(hex)
 cLinterp(hex1, hex2, percentage)
 cInvert(hex)
@@ -35,6 +35,16 @@ function cBreakdown(hexRGBA) {
 		b: parseInt(hex.slice(3, 5), 16),
 		g: parseInt(hex.slice(5, 7), 16),
 		a: (hexRGBA.length > 7) ? parseInt(hex.slice(7, 9), 16) / 255 : 1
+	};
+}
+
+function cBreakdownRGBA(rgbaString) {
+	var split = rgbaString.split(" ");
+	return {
+		r: +split[0].slice(5, -1),
+		g: +split[1].slice(0, -1),
+		b: +split[2].slice(0, -1),
+		a: +split[3].slice(0, -1)
 	};
 }
 
@@ -78,4 +88,87 @@ function cInvert(hex) {
 	var finB = 255 - parseInt(hex.slice(5, 7), 16);
 	//converting back to hex
 	return ("#" + Math.floor(finR).toString(16).padStart(2, "0") + Math.floor(finG).toString(16).padStart(2, "0") + Math.floor(finB).toString(16).padStart(2, "0"));
+}
+
+/**
+ * takes in an RGB object, and returns an HSV object
+ * @param {Object} RGBObj an object that contains r, g, and b properties
+ */
+function HSVtoRGB(hsvObj) {
+	if (hsvObj.h < 0) {
+		hsvObj.h += 360;
+	}
+	//I don't understand most of this but it appears to work
+	var compound = hsvObj.v * (hsvObj.s / 100);
+	var x = compound * (1 - Math.abs(((hsvObj.h / 60) % 2) - 1));
+	var mystery = hsvObj.v - compound;
+	var RGB = [0, 0, 0];
+
+	switch(Math.floor(hsvObj.h / 60)) {
+		case 0:
+			RGB = [compound, x, 0];
+			break;
+		case 1:
+			RGB = [x, compound, 0];
+			break;
+		case 2:
+			RGB = [0, compound, x];
+			break;
+		case 3:
+			RGB = [0, x, compound];
+			break;
+		case 4:
+			RGB = [x, 0, compound];
+			break;
+		case 5:
+			RGB = [compound, 0, x];
+			break;
+	}
+
+	RGB = [
+		Math.floor((RGB[0] + mystery) * 255), 
+		Math.floor((RGB[1] + mystery) * 255), 
+		Math.floor((RGB[2] + mystery) * 255)
+	];
+
+	return {
+		r: RGB[0],
+		g: RGB[1],
+		b: RGB[2],
+		a: hsvObj.a
+	};
+}
+
+function RGBtoHSV(rgbObj) {
+	var r = rgbObj.r / 255;
+	var g = rgbObj.g / 255;
+	var b = rgbObj.b / 255;
+	var cMax = Math.max(r, g, b);
+	var cMin = Math.min(r, g, b);
+	var delta = cMax - cMin;
+
+	var hsvObj = {
+		h: 0,
+		s: (cMax == 0) ? 0 : delta / cMax,
+		v: cMax,
+		a: rgbObj.a
+	};
+
+	//hue calculation
+	if (delta == 0) {
+		hsvObj.h = 0;
+	} else {
+		switch (cMax) {
+			case r:
+				hsvObj.h = 60 * (((g - b) / delta) % 6);
+				break;
+			case g:
+				hsvObj.h = 60 * ((b - r) / delta + 2);
+				break;
+			case b:
+				hsvObj.h = 60 * ((r - g) / delta + 4);
+				break;
+		}
+	}
+	return hsvObj;
 }
