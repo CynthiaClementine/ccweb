a41de4e9df1591528705495cb569954fd24be4ef
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2023-08-23 20:25:17 -0400

    Improved height consistency across refresh rates

--- gtteatine/objects.js
+++ gtteatine/objects.js
@@ -53,7 +53,7 @@ class Player {
 		//coordinates
 		var bridgeWidth = tileWidth * (sliceTiles - 0.25) * 0.5;
 		
-		this.y += this.dy;
+		this.y += this.dy * dt;
 		//dy is split in half so it's more precisely integrated
 		this.dy -= activeAY * dt * 0.5;
 		if (!this.dead) {
