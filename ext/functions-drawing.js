f0488871a7a98c13ee26256e4e2278e4cd614834
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2023-09-27 09:54:31 -0400

    Added background drawing

--- ext/functions-drawing.js
+++ ext/functions-drawing.js
@@ -1,3 +1,14 @@
+/*
+INDEX
+
+drawCircle(x, y, r, color)
+drawLine(x1, y1, x2, y2, color)
+drawMenu()
+
+rasterizeBG
+*/
+
+
 
 
 function drawCircle(x, y, r, color) {
@@ -13,6 +24,19 @@ function drawLine(x1, y1, x2, y2, color) {
 	ctx.moveTo(x1, y1);
 	ctx.lineTo(x2, y2);
 	ctx.stroke();
+}
+
+function drawText(text, x, y, font, color, alignment) {
+	if (font) {
+		ctx.font = font;
+	}
+	if (color) {
+		ctx.fillStyle = color;
+	}
+	if (alignment) {
+		ctx.textAlign = alignment;
+	}
+	ctx.fillText(text, x, y);
 }
 
 
@@ -34,15 +58,13 @@ function drawMenu() {
 
 	ctx.strokeStyle = color_menuText;
 	ctx.lineWidth = canvas.height / 200;
-	ctx.fillStyle = color_menuText;
 
 	//draw volume sliders
 	var sliderW = cs*3;
 	var sliderStart1 = baseW + cs*2.75;
 	var sliderStart2 = baseW + cs * (menu_boxSize[0]/2 + 2.85);
-	ctx.textAlign = "left";
-	ctx.font = `${canvas.height / 30}px Playfair Display`;
-	ctx.fillText(`Music Volume`, baseW, baseH + cs);
+
+	drawText(`Music Volume`, baseW, baseH + cs, `${canvas.height / 30}px Playfair Display`, color_menuText, "left");
 	ctx.fillText(`Effects Volume`, baseW + cs*(menu_boxSize[0]/2), baseH + cs);
 
 	//aliasing checkbox
@@ -72,8 +94,6 @@ function drawMenu() {
 	
 	
 	//controls + credits
-	ctx.fillStyle = color_menuText;
-	
 	var baseTextH = baseH + (cs * menu_boxSize[1] * 0.5);
 	
 	ctx.fillText(`Walk North - Up arrow / W`, baseW, baseTextH + cs*0.5);
@@ -101,4 +121,79 @@ function drawMenu() {
 
 	//swap back
 	[cty, ctx] = [ctx, cty];
+}
+
+
+
+
+function rasterizeBG() {
+	//figure out dimensions of the canvas
+	var chunkPxW = bg_tw * bg_chunkW;
+	var chunkPxH = bg_tw * vScale * bg_chunkH;
+
+	bg_canvas.width = chunkPxW * bg_chunkArr[0].length;
+	bg_canvas.height = chunkPxH * bg_chunkArr[1].length;
+	btx.imageSmoothingEnabled = false;
+
+	for (var y=0; y<bg_chunkArr.length; y++) {
+		for (var x=0; x<bg_chunkArr[0].length; x++) {
+			drawChunkToBG(x, y, chunkPxW, chunkPxH);
+		}
+	}
+	bg_chunkArr
+}
+
+function drawChunkToBG(chunkX, chunkY, chunkPxW, chunkPxH) {
+	//if there's no data there don't bother drawing
+	if (bg_chunkArr[chunkY][chunkX] == undefined) {
+		return;
+	}
+
+	//if the image hasn't loaded yet, wait until it does
+	if (bg_chunkArr[chunkY][chunkX].width == 0) {
+		window.setTimeout(() => {
+			drawChunkToBG(chunkX, chunkY, chunkPxW, chunkPxH);
+		}, 10);
+		return;
+	}
+
+	//all good? Draw the thing
+	var drawX = chunkPxW * chunkX;
+	var drawY = chunkPxH * chunkY;
+
+	btx.drawImage(bg_chunkArr[chunkY][chunkX], drawX, drawY);
+}
+
+function drawWorldBG() {
+	//solid bg color
+	ctx.fillStyle = `hsl(${dt_tLast * 0.01}, 50%, 30%)`;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	//actual world background
+
+
+	var bg_th = bg_tw * vScale;
+	var bgScreenRatio = bg_tw / camera.scale;
+
+	//distance from the canvas origin to the world origin, in tiles
+	var origTransX = bg_chunkStart[0] * bg_chunkW;
+	var origTransY = bg_chunkStart[1] * bg_chunkH;
+
+	var origTransCoords = spaceToScreen(origTransX, origTransY);
+	var edgeCoords = screenToSpace(0, 0);
+
+	//sWidth is in canvas pixels, or more specifically (world tiles * canvas pixels / world tile)
+	//well canvas pixels/world tile is already set - it's bg_tw
+	//camera.scale = (screen pixels / world tile)
+
+	// var sx = -((origTransCoords[0]) * bgScreenRatio)// + (origTransX * bg_tw * bgScreenRatio);
+	// var sy = -((origTransCoords[1]) * bgScreenRatio)// + (origTransY * bg_th * bgScreenRatio);
+	var sx = (edgeCoords[0] - origTransX) * bg_tw;
+	var sy = (edgeCoords[1] - origTransX) * bg_th;
+	var sw = camera.targetWidth * bg_tw;
+	var sh = (canvas.height / (camera.scale * vScale)) * bg_th;
+
+	// console.log(origTransX, origTransY, sx, sy, sw, sh);
+
+	ctx.drawImage(bg_canvas, sx, sy, sw, sh, 0, 0, canvas.width, canvas.height)
 }
