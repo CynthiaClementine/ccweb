e38d0585173383301d1149b89b60a17d414ac18a
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2024-01-17 12:31:23 -0500

    Added circleCircleIntersect and lineLineClosest

--- common/collision-2d.js
+++ common/collision-2d.js
@@ -2,8 +2,10 @@
 
 INDEX
 getOrientation(p1, p2, p3)
+circleCircleIntersect(circleParams1, circleParams2)
 lineIntersect(lin1p1, lin1p2, lin2p1, lin2p2)
 lineLineIntersect(l1p1, l1p2, l2p1, l2p2)
+lineLineClosestPoints(l1p1, l1p2, l2p1, l2p2)
 inPoly(xyPoint, polyPoints)
 pointLineIntersect(p, linp1, linp2, tolerance)
 pointLineDistance(point, lineP1, lineP2)
@@ -19,6 +21,59 @@ function getOrientation(p1, p2, p3) {
 	
 	//If second slope is greater than 0, clockwise. If second slope is smaller, counterclockwise. If not, it must be colinear.
 	return Math.sign(value);
+}
+
+//this code yoinked from the interwebs. I'm lazy. 
+//https://stackoverflow.com/questions/12219802/a-javascript-function-that-returns-the-x-y-points-of-intersection-between-two-ci
+//circleParams in [x, y, r] form
+function circleCircleIntersect(circleParams1, circleParams2) {
+	var [x0, y0, r0] = circleParams1;
+	var [x1, y1, r1] = circleParams2;
+	var h, rx, ry;
+
+	//get distances between circle centers
+	var dx = x1 - x0;
+	var dy = y1 - y0;
+
+	//distance
+	var dist = Math.sqrt(dx * dx + dy * dy);
+
+	//if circles do not intersect (are too far apart)
+	if (dist > (r0 + r1)) {
+		return undefined;
+	}
+	//if circles are contained within each other
+	if (dist < Math.abs(r0 - r1)) {
+		return undefined;
+	}
+
+	//'point 2' is the point where the line through the circle intersection points crosses the line between the circle centers.
+
+	//Determine the distance from point 0 to point 2.
+	var p02Dist = ((r0 * r0) - (r1 * r1) + (dist * dist)) / (dist * 2);
+
+	//Determine the coordinates of point 2.
+	var x2 = x0 + (dx * p02Dist / dist);
+	var y2 = y0 + (dy * p02Dist / dist);
+
+	/* Determine the distance from point 2 to either of the
+	* intersection points.
+	*/
+	h = Math.sqrt((r0 * r0) - (p02Dist * p02Dist));
+
+	/* Now determine the offsets of the intersection points from
+	* point 2.
+	*/
+	rx = -dy * (h / dist);
+	ry = dx * (h / dist);
+
+	/* Determine the absolute intersection points. */
+	var xi = x2 + rx;
+	var xi_prime = x2 - rx;
+	var yi = y2 + ry;
+	var yi_prime = y2 - ry;
+
+	return [[xi, yi], [xi_prime, yi_prime]];
 }
 
 /*lines intersect if the orientation between:
@@ -51,7 +106,42 @@ function lineLineIntersect(l1p1, l1p2, l2p1, l2p2) {
 		return [t1, 1 - t2];
 	}
 	return [];
-};
+}
+
+//returns the two closest points of two lines. If the two lines intersect, the points will be the same. 
+//If the lines are parallel, returns one of the endpoints of the line. 
+function lineLineClosestPoints(l1p1, l1p2, l2p1, l2p2) {
+	var r = [l2p1[0] - l1p1[0], l2p1[1] - l1p1[1]];
+	var u = [l1p2[0] - l1p1[0], l1p2[1] - l1p1[1]];
+	var v = [l2p2[0] - l2p1[0], l2p2[1] - l2p1[1]];
+
+	var ru = r[0] * u[0] + r[1] * u[1];
+	var rv = r[0] * v[0] + r[1] * v[1];
+	var uu = u[0] * u[0] + u[1] * u[1];
+	var uv = u[0] * v[0] + u[1] * v[1];
+	var vv = v[0] * v[0] + v[1] * v[1];
+
+	var det = uu * vv - uv * uv;
+	var s, t;
+
+	if (det < 0.001 * uu * vv) {
+		s = ru / uu;
+		s = (s < 0) ? 0 : (s > 1) ? 1 : s;
+		t = 0;
+	} else {
+		s = (ru * vv - rv * uv) / det;
+		s = (s < 0) ? 0 : (s > 1) ? 1 : s;
+		t = (ru * uv - rv * uu) / det;
+		t = (t < 0) ? 0 : (t > 1) ? 1 : t;
+	}
+
+	var sp = (t * uv + ru) / uu;
+	sp = (sp < 0) ? 0 : (sp > 1) ? 1 : sp;
+	var tp = (s * uv - rv) / vv;
+	tp = (tp < 0) ? 0 : (tp > 1) ? 1 : tp;
+
+	return [[l1p1[0] + sp * u[0], l1p1[1] + sp * u[1]], [l2p1[0] + tp * v[0], l2p1[1] + tp * v[1]]];
+}
 
 
 function inPoly(xyPoint, polyPoints) {
