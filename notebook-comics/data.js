459725571985014e659bff4caa2d67a9e795b0e5
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2024-12-25 15:12:48 -0500

    Added

---
+++ notebook-comics/data.js
@@ -0,0 +1,143 @@
+var data = {
+	"pmmm": {
+		path: `IMG_6135.jpg`,
+		title: `the entire plot of pmmm`,
+		info: `these are just going to be hard to read due to my awful handwriting. Unfortunately I'm not changing these.`
+	}, "dune-lamp": {
+		path: `IMG_6136.jpg`,
+		title: `floating lamp from dune`,
+		info: `wow. It's the floating lamp from dune`
+	}, "wonton": {
+		path: `IMG_6137.jpg`,
+		title: `Big Wonton`,
+		info: `the Morse code is just from a song someone was playing at the time. It's not related to the comic.`
+	}, "stacker": {
+		path: `IMG_6138.jpg`,
+		title: `object stacker gf`,
+		info: `she's not anybody's girlfriend she's just gluten free`
+	}, "drawings": {
+		path: `IMG_6139.jpg`,
+		title: `lamp and bush`,
+		info: ``
+	}, "47": {
+		path: `IMG_6145.jpg`,
+		title: `47`,
+		info: `and they were. friends (:`
+	}, "pretty": {
+		path: `IMG_6146.jpg`,
+		title: `pretty`,
+		info: `this is also funny if you take just the first two panels`
+	}, "shapeshifter": {
+		path: `IMG_6147.jpg`,
+		title: `shapeshifter`,
+		info: `I think shapeshifting would be a really terrible power to have, because what if I changed something and then didn't know how to change back? Do you really know exactly how think your arm is? Would you be able to perfectly return your facial features? Scary to think about`
+	}, "turtle": {
+		path: `IMG_6148.jpg`,
+		title: `abandonment`,
+		info: `ok I've been told this one doesn't make sense. So you know when you're engaged in an activity with someone and then it becomes apparent that they don't care about the activity half as much as you do? This is that. ...it should really be at least 6 panels.`
+	}, "bathrooms": {
+		path: `IMG_6150.jpg`,
+		title: `creepy or wet`,
+		info: `>it's a good bathroom`
+	}, "wild": {
+		path: `IMG_6154.jpg`,
+		title: `wild`,
+		info: `it's an outer wilds reference. Imagine this happening in dark bramble`
+	}, "castle": {
+		path: `IMG_6156.jpg`,
+		title: `Untitled 4`,
+		info: `what do you even do at that point? You can't really go back`
+	}, "worm": {
+		path: `IMG_6157.jpg`,
+		title: `worm`,
+		info: `what, you read all that text and you want to read more? Why?`
+	}, "baked": {
+		path: `IMG_6158.jpg`,
+		title: `baked`,
+		info: `you have to realize a good chunk of these are more to keep *me* entertained than anything else`
+	}, "cbh": {
+		path: `IMG_6159.jpg`,
+		title: `Cubic Black Hole`,
+		info: `that's so cool. Why don't you try getting closer`
+	}, "unhappy": {
+		path: `IMG_6182.jpg`,
+		title: `unhappiness`,
+		info: `every once in a while I have to throw in something to showcase my credibility`
+	}, "goose": {
+		path: `IMG_6183.jpg`,
+		title: `how to goose`,
+		info: `we're still not sure `
+	}, "celestial": {
+		path: `IMG_6184.jpg`,
+		title: `celestial bodies`,
+		info: `that green comet isn't actually my favorite, but I wanted the excuse to give a long name.`
+	}, "rockets": {
+		path: `IMG_6277.jpg`,
+		title: `vertical real estate`,
+		info: `rockets!!!!!`
+	}, "kissing": {
+		path: `IMG_6278.jpg`,
+		title: `kissing`,
+		info: `I don't understand kissing. It's in media everywhere as this big symbol of love but I just don't enjoy it at all. It baffles me.`
+	}, "hair-color": {
+		path: `IMG_6279.jpg`,
+		title: `hair color`,
+		info: `wow, because it's in grayscale so you can't see the color. Real groundbreaking concepts here`
+	}, "perspective": {
+		path: `IMG_6280.jpg`,
+		title: `unfinished perspective`,
+		info: `I drew this at work but then I got tired of drawing, and work ended, so I never ended up finishing this one.`
+	}, "thame-jame-1": {
+		path: `IMG_6281.jpg`,
+		title: `The adventures of Thame and Jame`,
+		info: `very short adventures`
+	}, "thame-jame-2": {
+		path: `IMG_6282.jpg`,
+		title: `The adventures of Thame and Jame (2nd edition)`,
+		info: ``
+	}, "alligator": {
+		path: `IMG_6283.jpg`,
+		title: `the story of the alligators`,
+		info: `I was upset about werewolf metaphors. So I added another metaphor, which surely will contribute to The Discourse.`
+	}, "angel-1": {
+		path: `IMG_6284.jpg`,
+		title: `You can't just put angels in something and expect it to make sense (part 1)`,
+		info: ``
+	}, "angel-2": {
+		path: `IMG_6285.jpg`,
+		title: `You can't just put angels in something and expect it to make sense (part 2)`,
+		info: `this one's not funny`
+	}, "biting": {
+		path: `IMG_6491.jpg`,
+		title: `biting`,
+		info: `I assume this is an experience everyone has had at least once`
+	}, "mods": {
+		path: `IMG_6497.jpg`,
+		title: `mods`,
+		info: `Literally incomprehensible. I'm so sorry`
+	}, "pocky": {
+		path: `IMG_6498.jpg`,
+		title: `pocky`,
+		info: `the angel and the devil are both girls they just have short hair for the aerodynamics`
+	}, "tree": {
+		path: `IMG_6509.jpg`,
+		title: `tree`,
+		info: `don't you hate it when this happens`
+	}, "men": {
+		path: `IMG_6510.jpg`,
+		title: `Why are there no men in your comics?`,
+		info: `creative process stymied by suddenly busy focus group`
+	}, "ptmyaa": {
+		path: `IMG_6549.jpg`,
+		title: `pills that make you anti-aliased`,
+		info: `for those of you upset that pixellation is spelled with two ls; Don't worry. It's british. or something`
+	}, "kermi": {
+		path: `IMG_6916.jpg`,
+		title: `Muppet Christmas Carol Shenanigans`,
+		info: `It's funny because she named herself brie, and ebeneezer thinks it could be undigested cheese as well. It's a whole connection. You get it? You get it??`
+	}, "god-name": {
+		path: `IMG_6918.jpg`,
+		title: `God's name`,
+		info: `it's blinking in panel 5 but I don't think that was communicated well`
+	},
+}
