2ed23dd58875a4f9261ab991ad89afcc2fa4cd90
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2025-02-19 19:37:48 -0500

    misc. development

--- svg-bullet-test/main.js
+++ svg-bullet-test/main.js
@@ -6,21 +6,47 @@ window.onmousemove = handleMouseMove;
 window.onkeydown = handleKeyPress;
 window.onkeyup = handleKeyRelease;
 
+window.addEventListener("load", () => {
+	console.log(`loaded!`);
+});
+
 var anim;
 
 var time_last = 0;
+var time_offset = 0;
 
 var cursor_x = 0;
 var cursor_y = 0;
 
 var pram_width = 640;
 var pram_height = 480;
+var pram_mode = "menu";
 
 var settings = {
 
 };
 
-var entities = [new Player(100, 100)];
+/*each stage consists of a set of timing points and entities.
+the entity data is a string following the format "type~args"
+where type determines what entity it is, and args are passed in as JSON-parsed arguments to the entity
+example: [1.2, `basic~`]
+will spawn a basic entity 50% of the way down the left edge, at t = 1.2
+
+
+basic~startX~startY~homeX~homeY
+*/
+var stages = {
+	"test": [
+		[1, "basic~-10~100~100~100"],
+		// [2, "basic~"],
+	]
+};
+
+var stage_current;
+var stage_boundaries = [[0, 0], [480, 480]];
+
+var player = new Player(100, 100);
+var entities = [player];
 var bullets_high = [];
 var bullets_low = [];
 
@@ -32,18 +58,67 @@ function setup() {
 }
 
 function main() {
-	var time = performance.now();
+	var time = (performance.now() / 1000) - time_offset;
 	//25ms max. after that just give up
-	var dt = Math.min(time - time_last, 25) / 1000;
+	var dt = Math.min(time - time_last, 25);
 	time_last = time;
 
-	//handle all entities
+	switch (pram_mode) {
+		case "stage":
+			runStage(stage_current, dt);
+			break;
+		case "menu":
+			break;
+	}
+
+	//handle general
+	anim = window.requestAnimationFrame(main);
+}
+
+function runStage(stageData, dt) {
+	var lastTime = time_last - dt;
+
+	//first figure out what part we're currently on
+	var line = 0;
+	while (line < stageData.length && stageData[line][0] < lastTime) {
+		line += 1;
+	}
+
+	//check if we have lines to process
+	//99.99% of the time this'll only be 0 or 1 lines, but one frame could theoretically cover multiple
+	while (line < stageData.length && hasTime(time_last, dt, stageData[line][0])) {
+		console.log(`processing line ${line}`);
+		for (var g=1; g<stageData[line].length; g++) {
+			stage_processEntity(stageData[line][g]);
+		}
+		line += 1;
+	}
+
+	//tick all the entities
 	entities.forEach(e => {
 		e.tick(dt);
 	});
+	bullets_high.forEach(b => {
+		b.tick(dt);
+	})
+}
 
-	//handle general 
-	anim = window.requestAnimationFrame(main);
+
+function stage_processEntity(entityData) {
+	var spl = entityData.split("~");
+
+	var constructor;
+	switch (spl[0]) {
+		case "basic":
+			constructor = Forest_Basic;
+			break;
+	}
+
+	spl = spl.slice(1).map(a => JSON.parse(a));
+	var ent = new constructor(...spl);
+	console.log(ent);
+	entities.push(ent);
+
 }
 
 
@@ -64,8 +139,8 @@ function handleMouseMove(e) {
 	cursor_x = clamp(cursor_x, 0, pram_width);
 	cursor_y = clamp(cursor_y, 0, pram_height);
 
-	φSet(workspace_zoomText, {
-		'innerHTML': `(${cursor_x}, ${cursor_y})`
+	φSet(readout_debug2, {
+		'innerHTML': `(${cursor_x.toFixed(2)}, ${cursor_y.toFixed(2)})`
 	});
 }
 
--- svg-bullet-test/objects.js
+++ svg-bullet-test/objects.js
@@ -1,33 +1,59 @@
-
-
 class Base {
-	constructor(x, y, spriteID) {
+	constructor(x, y, r, spriteID) {
 		this.x = x;
 		this.y = y;
+		//this.r marks radius for collision, not necessarily for visuals. Sprite radius is different
+		this.r = r;
+		this.age = 0;
+		this.spriteID = spriteID;
 		this.sprite = φCreate("use", {
-			'x': x,
-			'y': y,
-			'user-select': 'none',
-			'href': `#${spriteID}`
+			x: x,
+			y: y,
+			"user-select": "none",
+			href: `#${spriteID}`,
 		});
 		workspace.appendChild(this.sprite);
+	}
+
+	clampPos() {
+		this.x = clamp(this.x, stage_boundaries[0][0], stage_boundaries[1][0]);
+		this.y = clamp(this.y, stage_boundaries[0][1], stage_boundaries[1][1]);
 	}
 
 	delete() {
 		workspace.removeChild(this.sprite);
 	}
-}
 
+	moveTo(x, y, scale, rotation) {
+		scale = scale ?? 1;
+		rotation = rotation ?? 0;
+		this.x = x;
+		this.y = y;
+		var effX = this.x / scale;
+		var effY = this.y / scale;
 
-
-
-class Player extends Base {
-	constructor(x, y) {
-		super(x, y, "player");
-		this.speed = 150;
+		φSet(this.sprite, {
+			x: effX,
+			y: effY,
+			transform: `rotate(${rotation} ${effX} ${effY}) scale(${scale})`,
+		});
 	}
 
 	tick(dt) {
+		this.age += dt;
+	}
+}
+
+class Player extends Base {
+	constructor(x, y) {
+		super(x, y, 1, "player");
+		this.speed = 150;
+		this.maxHealth = 3;
+		this.health = 3;
+	}
+
+	tick(dt) {
+		super.tick(dt);
 		//move towards the mouse.
 		var speed = this.speed * dt;
 
@@ -35,26 +61,124 @@ class Player extends Base {
 		var cpos = [cursor_x, cursor_y];
 		var cvec = d2_subtract(cpos, pos);
 
-		if (distSquared(...cvec) < speed * speed) {
-			return;
+		if (distSquared(...cvec) >= speed * speed) {
+			cvec = d2_scaleBy(d2_normalize(cvec), speed);
+		} else {
+			cvec = [0, 0];
 		}
-
-		cvec = d2_scaleBy(d2_normalize(cvec), speed);
 
 		this.x += cvec[0];
 		this.y += cvec[1];
 
-		φSet(this.sprite, {
-			'x': this.x,
-			'y': this.y,
-		})
+		this.clampPos();
+
+		this.moveTo(this.x + cvec[0], this.y + cvec[1], 1);
 	}
 }
 
+class Enemy extends Base {
+	constructor(x, y, spriteBaseID, homeX, homeY) {
+		super(x, y, spriteBaseID);
+
+		this.home = [homeX, homeY];
+		this.start = [x, y];
+
+		this.introTime = 1;
+	}
+
+	posIntro(t) {
+		t = (1 - t) ** 2;
+
+		return linterpMulti(this.home, this.start, t);
+	}
+
+	//pos
+	posMain(t) {
+		return this.home;
+	}
+
+	calculatePos() {
+		var targetPos;
+		if (this.age < this.introTime) {
+			targetPos = this.posIntro(this.age);
+		} else {
+			targetPos = this.posMain(this.age - this.introTime);
+		}
+
+		//if the position is undefined in any way, it's probably time to die
+		if (!targetPos || !targetPos[0]) {
+			this.die();
+			return;
+		}
+
+		this.moveTo(...targetPos);
+	}
+
+	fire(dt) {
+		if (hasTimeCycle(this.age, dt, 1, 0.5)) {
+			cbFlower(this.x, this.y, 4, 5, Math.random(), )
+		}
+	}
+
+	die() {
+		console.log(`dying`);
+		//remove self from the entities array
+		entities.splice(entities.indexOf(this), 1);
+	}
+
+	tick(dt) {
+		super.tick(dt);
+		this.calculatePos();
+		this.fire(dt);
+	}
+}
+
+class Forest_Basic extends Enemy {
+	constructor(startX, startY, homeX, homeY) {
+		super(startX, startY, "forest_woody", homeX, homeY);
+	}
+}
+
+// class Enemy
+
 class Bullet extends Base {
-	constructor(x, y, dx, dy) {
-		super(x, y);
+	constructor(x, y, r, spriteType, dx, dy) {
+		super(x, y, "bullet_" + spriteType);
 		this.dx = dx;
 		this.dy = dy;
+		this.r = r;
+		this.angle = Math.atan2(this.dy, this.dx);
+	}
+
+	collide() {
+		var largeBox = this.r * 2;
+		//check for proximity to player
+		if (Math.abs(this.x - player.x) > largeBox || Math.abs(this.y - player.y) > largeBox) {
+			return;
+		}
+
+		if (distSquared() < this.r * this.r) {
+			//collision!
+			player.beHit();
+			//destroy self
+		}
+	}
+
+	tick(dt) {
+		//move by x, y
+		this.moveTo(
+			this.x + this.dx * dt,
+			this.y + this.dy * dt,
+			this.r,
+			this.angle,
+		);
+		this.collide();
+		console.log(`hi`);
+	}
+}
+
+class Bullet_Complex extends Bullet {
+	constructor(x, y, r, spriteType, pathFunc) {
+		super(x, y, 0, 0);
 	}
 }
