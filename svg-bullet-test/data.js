81057fcda8ac3698261fa3d8431863bc8bd5f053
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2025-02-19 19:36:23 -0500

    Initial

---
+++ svg-bullet-test/data.js
@@ -0,0 +1,53 @@
+
+var conversations = {
+	"intro": [
+		`Welcome! This is the tutorial, in which you can learn how to play. To advance text, press Z.`,
+		`The fine fellow following your cursor around is Skonk, the star of the show! She is on a grand quest that involves many obstacles.`,
+		`You may learn more about that later. Or not.`,
+		`Asi`
+
+		//talk about movement, attacking
+		//talk about grazing, dodging, health
+		//talk about enemies
+	],
+
+	//dark forest (wow! it's like a ph reference)
+	"forest-start": [
+		`Skonk awoke on a morning like any other to find that her precious berry cache had gone missing.
+		This was by far the worst thing to happen to her since 1955.`,
+		`She immediately vowed to find the berries and get them back. To start, she ventured into the nearby forest.`,
+		`However, the creatures dwelling there were not friendly. The journey would be difficult.`
+	],
+	"forest-mid": [
+
+	],
+	"forest-preboss": [
+		`[blue]Who goes there?`,
+		`A voice boomed from across the field.`,
+		`[brown]hi,, i'm here to look for my berries,,,`,
+		`The Largest Lizard revealed himself.`,
+		`[blue]Well that's a shame. I haven't seen any of your berries here. I only see all of my berries here!`,
+		`[blue]Of course, every berry I see is mine.`,
+		`It was clear there would be no bargaining.`,
+	],
+	"forest-postboss": [
+		`With the Largest Lizard defeated, berries could be given to all the forest creatures.`,
+		`However, despite Skonk's best efforts, she could not find her own berries amongst the pile. They must be elsewhere.`,
+		`So the search continues.`
+	],
+
+	//school
+	"school-start": [
+
+	],
+	"school-mid": [
+
+	],
+	"school-preboss": [
+
+	],
+	"school-postboss": [
+		`So that really was just a colossal waste of time. `
+	],
+
+}
